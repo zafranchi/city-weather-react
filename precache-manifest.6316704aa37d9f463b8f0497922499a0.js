3f7856790a0f174ffbde696f7550c13569891d5a
Ailar Zafranchi <ailar.zafranchi@gmail.com>
2019-11-29 19:19:25 -0500

    Updates

---
+++ precache-manifest.6316704aa37d9f463b8f0497922499a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476fc513d87251779d91505700157459",
+    "url": "/city-weather-react/index.html"
+  },
+  {
+    "revision": "74fd4ecb9a3067e8ba17",
+    "url": "/city-weather-react/static/css/main.5df64f12.chunk.css"
+  },
+  {
+    "revision": "606946e5c29266b3ed7c",
+    "url": "/city-weather-react/static/js/2.e4811ccb.chunk.js"
+  },
+  {
+    "revision": "74fd4ecb9a3067e8ba17",
+    "url": "/city-weather-react/static/js/main.393c8a31.chunk.js"
+  },
+  {
+    "revision": "877309fa2879b4e8779c",
+    "url": "/city-weather-react/static/js/runtime-main.534237b4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/city-weather-react/static/media/logo.25bf045c.svg"
+  }
+]);
