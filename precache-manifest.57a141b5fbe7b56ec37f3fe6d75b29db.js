d81a717a47378de5687667fb93d139ab0a620980
Ailar Zafranchi <ailar.zafranchi@gmail.com>
2019-12-08 18:30:50 -0500

    Updates

---
+++ precache-manifest.57a141b5fbe7b56ec37f3fe6d75b29db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb3211b5cf7a9d5f9d07fa1f7abdc69",
+    "url": "/city-weather-react/index.html"
+  },
+  {
+    "revision": "97f5ab861cb9708394fe",
+    "url": "/city-weather-react/static/css/main.5d47924a.chunk.css"
+  },
+  {
+    "revision": "d4aeda5c287ee3898359",
+    "url": "/city-weather-react/static/js/2.20a5d5d3.chunk.js"
+  },
+  {
+    "revision": "97f5ab861cb9708394fe",
+    "url": "/city-weather-react/static/js/main.67a86099.chunk.js"
+  },
+  {
+    "revision": "877309fa2879b4e8779c",
+    "url": "/city-weather-react/static/js/runtime-main.534237b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/city-weather-react/precache-manifest.b8f8628df3b0ba9ae2e6f33043edd178.js"
+  "/city-weather-react/precache-manifest.57a141b5fbe7b56ec37f3fe6d75b29db.js"
 );
 
 self.addEventListener('message', (event) => {
